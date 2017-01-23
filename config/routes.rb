28012582178e123c97344adade665823e0933830
Brian Ramirez <bramirez@umass.edu>
2017-01-23 14:12:06 -0500

    added devise and basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :links
+
+  # The root is set to the Links Controller's index action
+  root to: "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
